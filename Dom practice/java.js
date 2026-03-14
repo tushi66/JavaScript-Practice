8fd30790e4a29cab4fc56906cf8818653ce8fa71
Tushar Patel <pateltusharsa@gmail.com>
2026-03-14 22:42:32 +0530

    Add DOM practice project

--- Dom practice/java.js
+++ Dom practice/java.js
@@ -1,17 +1,17 @@
 // dom Taget methods
 
-let dom = document.getElementById("container");
+// let dom = document.getElementById("container");
 
-console.log(dom);
+// console.log(dom);
 
 
-dom.textContent = "Welocme to the page!!!";
+// dom.textContent = "Welocme to the page!!!";
 
-dom.innerHTML = ` 
-    <h1> Hello, HI  </h1>
-    <p> Tushar Patel </p>
-    <h2>Hiii Mr. Welocme to the Page</h2>
-`
+// dom.innerHTML = ` 
+//     <h1> Hello, HI  </h1>
+//     <p> Tushar Patel </p>
+//     <h2>Hiii Mr. Welocme to the Page</h2>
+// `
 
 // dom.textContent=
 // ` 
@@ -21,13 +21,63 @@ dom.innerHTML = `
 // `
 
 
-let value = document.getElementsByClassName("heading");
+// let value = document.getElementsByClassName("heading");
+
+// console.log(value);
+
+// dom.style.backgroundColor = "red";
+// dom.style.color = "white";
+// dom.style.border= "2px solid black"
+// dom.style.textAlign= "center";
+
+for(let i=0; i < 2; i++){
+    
+    let dom = document.getElementsByClassName("heading")[i];
+
+    console.log(dom);
+
+    
+    dom.style.backgroundColor = "red";
+    dom.style.color = "white";
+    dom.style.border= "2px solid black"
+    dom.style.textAlign= "center";
+        
+
+}
+
+
+let value  = document.getElementsByTagName("p")[0];
 
 console.log(value);
 
-dom.style.backgroundColor = "red";
-dom.style.color = "white";
-dom.style.border= "1px solid black"
+
+value.style.backgroundColor = "blue";
+value.style.color = "white";
+value.style.padding = "10px";
+value.style.border="4px solid red"
 
 
+let dom1 = document.getElementsByName("firstname")[0];
+let dom2 = document.getElementsByName("lastname");
 
+console.log(dom1);
+console.log(dom2);
+
+dom1.value = "My name is tushar";   
+
+//quary selcetor & quaryselctorall
+
+
+function select(){
+
+    const title= document.querySelector(".heading1");
+
+
+    title.style.backgroundColor = "green";
+    title.style.color = "white";
+    title.style.padding="10px";
+
+    
+    
+
+}
